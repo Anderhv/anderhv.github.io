c4e70413dbba873b32ec1bc16dd4c5366b7e8729
AndersHV <anderhv@stud.ntnu.no>
2018-06-02 23:18:31 -0700

    Functional calls to database

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 
 window.onload = function() {
-	console.log('JavaScript loaded')
+	var form = document.getElementById("asdo");
+	console.log(form);
 	
-};
+var firebaseref = firebase.database().ref();
 
+form.onsubmit = function() {
+	var elements = form.elements;
+	for (var i = 0, element; element = elements[i++];) {
+		if (element.type === "radio" && element.checked)
+			firebaseref.push().set(element.value)
+
+	}
+
+    //var ans1 = form.elements["guitar1"];
+    //var ans2 = form.elements["guitar2"];
+    //var ans1 = form.elements["guitar1"];
+    //var ans1 = form.elements["guitar1"];
+   	//var ans1 = form.elements["guitar1"];
+
+	//firebaseref.child("Ans1").set(ans1.value)
+	window.alert("Answer submitted!")
+};
 
 function init() {
 	console.log("Initializing")
@@ -16,7 +34,7 @@ function playAudio() {
 	playAudioz(asd)
 } 
 
-
+	
 function playAudioz(sound) {
 	//var asd = document.getElementById("Fender");
 	sound.play()
@@ -28,3 +46,5 @@ function pauseAudio() {
 
     asd.pause(); 
 } 
+
+};
